6179ffb573309f63fde7ceb3b04e1819882231f5
yain09 <yain09@gmail.com>
2024-05-30 14:48:50 -0300

    fix url

--- src/scripts/Arquitectura.jsx
+++ src/scripts/Arquitectura.jsx
@@ -8,13 +8,21 @@ function Arquitectura() {
       <Row className="text-start font-monospace align-items-end ">
         <Col className="arq">
           <div className="arq">
-            {location.pathname === "/Portfolio" && (
+            {(location.pathname === "/Portfolio" || location.pathname === "/Portfolio/") && (
               <p className="fs-5 m-0 p-0">portfolio</p>
             )}
-            {location.pathname === "/model" && <p className="fs-5 m-0 p-0">modelado 3D</p>}
-            {location.pathname === "/employee" && <p className="fs-5 m-0 p-0">en relación de dependencia</p>}
-            {location.pathname === "/concursos" && <p className="fs-5 m-0 p-0">concursos</p>}
-            {location.pathname === "/freelance" && <p className="fs-5 m-0 p-0">independiente</p>}
+            {location.pathname === "/model" && (
+              <p className="fs-5 m-0 p-0">modelado 3D</p>
+            )}
+            {location.pathname === "/employee" && (
+              <p className="fs-5 m-0 p-0">en relación de dependencia</p>
+            )}
+            {location.pathname === "/concursos" && (
+              <p className="fs-5 m-0 p-0">concursos</p>
+            )}
+            {location.pathname === "/freelance" && (
+              <p className="fs-5 m-0 p-0">independiente</p>
+            )}
             <span className="fs-1 m-0 p-0">Yain Godoy </span>
             <span>arquitecto </span>
             <div className="arq"></div>
